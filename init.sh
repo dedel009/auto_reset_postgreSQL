99092766c86724d0a26cca7481885ea973447f7c
kai <m01056994380@gmail.com>
2024-07-16 14:50:54 +0900

    init.sh 수정

--- init.sh
+++ init.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e
 
-# PostgreSQL 서버가 시작될 때까지 대기
-until pg_isready -h localhost; do
-  sleep 1
-done
+echo "Running pg_restore"
 
 # SQL 스크립트 실행
-# psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/restore.sql
-# 스키마만 복원
-pg_restore -U ronfic --schema-only -v -d ronfic_main /docker-entrypoint-initdb.d/restore.sql
-# 데이터만 복원
-pg_restore -U ronfic --data-only -j 8 -v -d ronfic_main /docker-entrypoint-initdb.d/restore.sql
+pg_restore -U "$POSTGRES_USER" -v -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/restore.sql
