07375860592305a2215f968a1173e6beb8129db2
kai <m01056994380@gmail.com>
2024-09-09 18:22:41 +0900

    ddl 추가

--- init.sh
+++ init.sh
@@ -5,3 +5,6 @@ echo "Running pg_restore"
 
 # SQL 스크립트 실행
 pg_restore -U "$POSTGRES_USER" -v -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/restore.sql
+
+pg_restore -U "$POSTGRES_USER" -v -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/separate_table_ddl.sql
+
