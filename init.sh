86f6f40a796f4033bd03c433afd16cce59e08fba
kai <m01056994380@gmail.com>
2024-09-10 12:22:51 +0900

    설정 update

--- init.sh
+++ init.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Running pg_restore"
+echo "Custom restore shell scripts"
 
 # SQL 스크립트 실행
-pg_restore -U "$POSTGRES_USER" -v -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/restore.sql
-
-pg_restore -U "$POSTGRES_USER" -v -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/separate_table_ddl.sql
+# pg_restore -U "$POSTGRES_USER" -v -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/restore.sql
 
